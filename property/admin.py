29d418c558b5238be52a9a4b643c79fbaddfb762
vladimir <vladimir@orb.com>
2022-01-21 09:45:17 +0500

    add list_display and list_editable for model Flat to admin site

--- property/admin.py
+++ property/admin.py
@@ -6,3 +6,6 @@ from .models import Flat
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner', 'construction_year')
+    list_display = ('town', 'address', 'price', 'is_new_building',
+                    'construction_year', 'owners_phonenumber')
+    list_editable = ('is_new_building',)
