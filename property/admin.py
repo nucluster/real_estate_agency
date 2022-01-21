01534872ce452a1ed1d2cc60bedc37962f72f2fd
vladimir <vladimir@orb.com>
2022-01-21 10:35:41 +0500

    add list_filter for model Flat to admin site

--- property/admin.py
+++ property/admin.py
@@ -9,3 +9,4 @@ class FlatAdmin(admin.ModelAdmin):
     list_display = ('town', 'address', 'price', 'is_new_building',
                     'construction_year', 'owners_phonenumber')
     list_editable = ('is_new_building',)
+    list_filter = ('is_new_building', 'number_of_rooms', 'has_balcony')
