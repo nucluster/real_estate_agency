ea0bfe6bde5a60d9495522c9c6f6c657e0b6d807
vladimir-asus <vladimir@vladimir-e200ha>
2022-01-28 17:02:51 +0500

    add inlines to m2m Flat-Owner

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class FlatInline(admin.TabularInline):
+    model = Flat.owners.through
+    raw_id_fields = ('flat', 'owner')
+
+
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'construction_year')
@@ -14,6 +19,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_display_links = ('id',)
     readonly_fields = ('created_at',)
     ordering = ('id',)
+    inlines = [FlatInline,]
 
 
 
@@ -29,4 +35,7 @@ class OwnerAdmin(admin.ModelAdmin):
     search_fields = ('fio',)
     list_display = ('id', 'fio', 'pure_phone_number')
     list_display_links = ('fio',)
+    ordering = ('id',)
+    inlines = [FlatInline,]
+    exclude = ('flats',)
 
