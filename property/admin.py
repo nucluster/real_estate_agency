72bd192940e25fd033aa78a52642fe0e7bbcea86
vladimir-asus <vladimir@vladimir-e200ha>
2022-01-28 16:48:42 +0500

    Remove owners fields from model Flat and castomize some feature in admin site

--- property/admin.py
+++ property/admin.py
@@ -5,13 +5,16 @@ from .models import Flat, Complaint, Owner
 
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('town', 'address', 'owner', 'construction_year')
-    list_display = ('town', 'address', 'price', 'is_new_building',
-                    'construction_year', 'owners_phonenumber',
-                    'owner_pure_phone')
+    search_fields = ('town', 'address', 'construction_year')
+    list_display = ('id', 'town', 'address', 'price', 'is_new_building',
+                    'construction_year')
     list_editable = ('is_new_building',)
     list_filter = ('is_new_building', 'number_of_rooms', 'has_balcony')
     raw_id_fields = ('likes',)
+    list_display_links = ('id',)
+    readonly_fields = ('created_at',)
+    ordering = ('id',)
+
 
 
 @admin.register(Complaint)
@@ -25,4 +28,5 @@ class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('flats',)
     search_fields = ('fio',)
     list_display = ('id', 'fio', 'pure_phone_number')
+    list_display_links = ('fio',)
 
